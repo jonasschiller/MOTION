b9af07c86e7c874da57d35113e8c4c442e55b362
Jonas Schiller <jonas.schiller@stud-mail.uni-wuerzburg.de>
2023-11-23 11:08:13 +0100

    Bugfix

--- src/examples/benchmark_multiplication/common/benchmark_multiplication.cpp
+++ src/examples/benchmark_multiplication/common/benchmark_multiplication.cpp
@@ -22,7 +22,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "benchmark_multiplications.h"
+#include "benchmark_multiplication.h"
 
 #include "algorithm/algorithm_description.h"
 #include "protocols/share_wrapper.h"
