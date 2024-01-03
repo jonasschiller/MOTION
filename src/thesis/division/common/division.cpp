4d4812ce05a516dded50b2220deef49c3749d340
Jonas Schiller <q533604@europe.bmw.corp>
2024-01-03 15:35:37 +0100

    Division

--- src/thesis/division/common/division.cpp
+++ src/thesis/division/common/division.cpp
@@ -104,9 +104,10 @@ encrypto::motion::ShareWrapper DummyBooleanGmwShare(encrypto::motion::PartyPoint
 encrypto::motion::RunTimeStatistics EvaluateProtocol(
     encrypto::motion::PartyPointer& party, std::size_t number_of_simd, std::size_t bit_size,
     encrypto::motion::MpcProtocol protocol) {
-  encrypto::motion::ShareWrapper a,b;
-  a = DummyBooleanGmwShare(party,32,1000000);
-  b = DummyBooleanGmwShare(party,32,1000000);
+  encrypto::motion::SecureUnsignedInteger a, b;
+  std::vector temporary(1000000);
+  a = party->In<encrypto::motion::MpcProtocol::kBooleanGmw>(temporary, 0);
+  b = party->In<encrypto::motion::MpcProtocol::kBooleanGmw>(temporary, 0);
   a=a/b;
   party->Run();
   party->Finish();
