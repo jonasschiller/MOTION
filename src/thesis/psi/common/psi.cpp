8f9843477db97d13f26aec3af62a4fc07a4abfad
Jonas Schiller <q533604@europe.bmw.corp>
2024-01-10 13:56:17 +0100

    PrivateSetIntersect

--- src/thesis/psi/common/psi.cpp
+++ src/thesis/psi/common/psi.cpp
@@ -22,7 +22,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "statistics.h"
+#include "psi.h"
 
 #include "algorithm/algorithm_description.h"
 #include "base/backend.h"
@@ -46,26 +46,18 @@ Stores the input attributes of party_0 and party_1.
 struct Attributes
 {
   std::vector<std::uint32_t> cleartext_input; // values for party_0 and categories for party_1.
-  std::vector<encrypto::motion::SecureUnsignedInteger> shared_input;
-  std::vector<encrypto::motion::SecureUnsignedInteger> id;
+  std::vector<encrypto::motion::SecureUnsignedInteger> ids;
 } party_0, party_1;
 
 /**
  * Stores all the inputs needed for StatisticCircuit().
  */
-struct StatisticContext
+struct PsiContext
 {
   Attributes party_0, party_1;
-  Results results;
+  encrypto::motion::ShareWrapper full_zero;
+  std::vector<encrypto::motion::SecureUnsignedInteger> results;
 };
-
-struct Results
-{
-  encrypto::motion::SecureUnsignedInteger mean;
-  encrypto::motion::SecureUnsignedInteger max;
-  encrypto::motion::SecureUnsignedInteger min;
-  encrypto::motion::SecureUnsignedInteger sum;
-} results;
 
 encrypto::motion::RunTimeStatistics EvaluateProtocol(
     encrypto::motion::PartyPointer &party, const std::string &input_file_path, encrypto::motion::MpcProtocol protocol)
@@ -79,27 +71,40 @@ encrypto::motion::RunTimeStatistics EvaluateProtocol(
       GetFileInput(party_id, input_file_path);
   party_0.cleartext_input = party_0_temp;
   party_1.cleartext_input = party_1_temp;
-  id = id_temp;
 
   for (std::size_t i = 0; i < party_0.cleartext_input.size(); i++)
   {
-    party_0.shared_input.push_back(
-        party->In<encrypto::motion::MpcProtocol::kArithmeticGmw>(party_0.cleartext_input[i], 0));
-    party_1.shared_input.push_back(party->In < encrypto::motion::MpcProtocol::kArithemticGmw(party_1.cleartext_input[i], 1));
     party_0.id.push_back(
-        party->In<encrypto::motion::MpcProtocol::kBooleanGmw>(encrypto::motion::ToInput(id[i]), 0));
+        party->In<encrypto::motion::MpcProtocol::kBooleanGmw>(encrypto::motion::ToInput(party_0.cleartext_input[i]), 0));
     party_1.id.push_back(
-        party->In<encrypto::motion::MpcProtocol::kBooleanGmw>(encrypto::motion::ToInput(id[i]), 1));
+        party->In<encrypto::motion::MpcProtocol::kBooleanGmw>(encrypto::motion::ToInput(party_1.cleartext_input[i]), 1));
   }
-  StatisticContext context{party_0, party_1, results};
-  Results output = CreateStatisticsCircuit(context);
-  // Constructs an output gate for each bin.
-  output.mean = output.mean.Out();
-  output.max = output.max.Out();
-  output.min = output.min.Out();
-  output.sum = output.sum.Out();
 
+  encrypto::motion::ShareWrapper full_zero = party->In<encrypto::motion::MpcProtocol::kBooleanGmw>(
+      encrypto::motion::BitVector<>(1, false), 0);
+  std::vector<encrypto::motion::SecureUnsignedInteger> result;
+  for (std::size_t i = 0; i < party_0.cleartext_input.size(); i++)
+  {
+    result.push_back(0);
+  }
+  PsiContext context{party_0, party_1, result, full_zero};
+  std::vector<encrypto::motion::SecureUnsignedInteger> output = CreatePsiCircuit(context);
+  // Constructs an output gate for each bin.
+  for (std::size_t i = 0; i < output.size(); i++)
+    output[i] = output[i].Out();
   party->Run();
+  // Converts the outputs to integers.
+  std::vector<std::uint32_t> result;
+  for (auto each_output : output)
+    result.push_back(each_output.As<std::uint32_t>());
+
+  if (print_output)
+  {
+    for (std::size_t i = 0; i < number_of_bins; i++)
+    {
+      std::cout << " " << i << "," << result[i] << std::endl;
+    }
+  }
   party->Finish();
   const auto &statistics = party->GetBackend()->GetRunTimeStatistics();
   return statistics.front();
@@ -112,70 +117,57 @@ std::tuple<std::vector<std::uint32_t>, std::vector<std::uint32_t>, std::vector<s
 GetFileInput(std::size_t party_id, const std::string &path, std::uint32_t number_of_bins)
 {
   std::ifstream infile;
-  std::vector<std::uint32_t> party_0, party_1, id;
+  std::vector<std::uint32_t> party_0, party_1;
   std::uint32_t n;
 
   infile.open(path);
   if (!infile.is_open())
-    throw std::runtime_error("Could not open CrossTabs file");
-
-  std::uint32_t i = 0;
+    throw std::runtime_error("Could not open Index file");
   while (infile >> n)
   {
     if (party_id == 0)
     {
       party_0.push_back(n); // Assigns input values to party_0.
-      party_1.push_back(n); // Assigns dummy input to party_1.
+      party_1.push_back(0); // Assigns dummy input to party_1.
     }
     else
     {
-      party_0.push_back(n); // Assigns dummy input to party_0..
+      party_0.push_back(0); // Assigns dummy input to party_0..
       party_1.push_back(n); // Assigns input values to party_1.
     }
-    id.push_back(i);
-    i++;
   }
   infile.close();
-  return {party_0, party_1, id};
+  return {party_0, party_1};
 }
 
 /**
  * Constructs the cross tabs of the given data in CrossTabsContext.
  * */
-std::vector<encrypto::motion::SecureUnsignedInteger> CreateCrossTabsCircuit(
-    CrossTabsContext context)
+std::vector<encrypto::motion::SecureUnsignedInteger> CreatePsiCircuit(
+    PsiContext context)
 {
-  auto party_0_id = context.party_0.id, party_0_values = context.party_0.shared_input,
-       party_1_id = context.party_1.id, party_1_categories = context.party_1.shared_input;
-  encrypto::motion::ShareWrapper id_match, bin_match;
+  auto party_0_id = context.party_0.id, ,
+       party_1_id = context.party_1.id,
+       encrypto::motion::ShareWrapper id_match;
 
-  for (std::size_t i = 0; i < party_0_id.size(); i++)
+  for (std::size_t i = 0; i < party_0.size(); i++)
   {
     for (std::size_t j = 0; j < party_1_id.size(); j++)
     {
       id_match = (party_0_id[i] == party_1_id[j]); // Checks if the indices are the same.
-      for (std::size_t k = 0; k < context.number_of_bins; k++)
-      {
-        // Checks if the bins numbers are the same.
-        bin_match = (context.categories[k] == party_1_categories[j]);
+      // Concatenates keep so that it has the same length as party_0_values[i].
+      std::vector<encrypto::motion::ShareWrapper> keep_concat;
+      keep_concat.push_back(keep);
+      for (std::size_t i = 0; i < 31; i++)
+        keep_concat.push_back(context.full_zero);
+      keep = encrypto::motion::ShareWrapper::Concatenate(keep_concat);
 
-        // Checks if both binmatch and idmatch are 'true'.
-        encrypto::motion::ShareWrapper keep = bin_match & id_match;
+      // Assigns party_0_values[i] in keep only if keep is 'true'.
+      keep = keep.Convert<encrypto::motion::MpcProtocol::kArithmeticGmw>();
+      keep = keep * party_0_id[i].Get();
 
-        // Concatenates keep so that it has the same length as party_0_values[i].
-        std::vector<encrypto::motion::ShareWrapper> keep_concat;
-        keep_concat.push_back(keep);
-        for (std::size_t i = 0; i < 31; i++)
-          keep_concat.push_back(context.full_zero);
-        keep = encrypto::motion::ShareWrapper::Concatenate(keep_concat);
-
-        // Assigns party_0_values[i] in keep only if keep is 'true'.
-        keep = keep.Convert<encrypto::motion::MpcProtocol::kArithmeticGmw>();
-        keep = keep * party_0_values[i].Get();
-
-        // Adds keep to the result.
-        context.results[k] = context.results[k] + encrypto::motion::SecureUnsignedInteger(keep);
-      }
+      // Adds keep to the result.
+      context.results.push_back(encrypto::motion::SecureUnsignedInteger(keep));
     }
   }
   return context.results;
