0d66e0b4d011ff14a323300d86b4f97b9a5eaff3
Jonas Schiller <q533604@europe.bmw.corp>
2024-01-26 16:00:49 +0100

    Bug Fixes

--- src/thesis/multiplication/common/multiplication.cpp
+++ src/thesis/multiplication/common/multiplication.cpp
@@ -29,13 +29,10 @@
 #include "base/register.h"
 #include "protocols/arithmetic_gmw/arithmetic_gmw_share.h"
 #include "protocols/arithmetic_gmw/arithmetic_gmw_wire.h"
-#include "protocols/bmr/bmr_share.h"
-#include "protocols/bmr/bmr_wire.h"
-#include "protocols/boolean_gmw/boolean_gmw_share.h"
-#include "protocols/boolean_gmw/boolean_gmw_wire.h"
 #include "protocols/share_wrapper.h"
 #include "statistics/analysis.h"
 #include "statistics/run_time_statistics.h"
+#include "secure_type/secure_unsigned_integer.h"
 #include "utility/block.h"
 #include "utility/config.h"
 
@@ -56,54 +53,6 @@ encrypto::motion::ShareWrapper DummyArithmeticGmwShare(encrypto::motion::PartyPo
 
   return encrypto::motion::ShareWrapper(
       std::make_shared<encrypto::motion::proto::arithmetic_gmw::Share<T>>(wires));
-}
-
-encrypto::motion::ShareWrapper DummyBmrShare(encrypto::motion::PartyPointer &party,
-                                             std::size_t number_of_wires,
-                                             std::size_t number_of_simd)
-{
-  std::vector<encrypto::motion::WirePointer> wires(number_of_wires);
-  const encrypto::motion::BitVector<> dummy_input(number_of_simd);
-
-  encrypto::motion::BackendPointer backend{party->GetBackend()};
-  encrypto::motion::RegisterPointer register_pointer{backend->GetRegister()};
-
-  for (auto &w : wires)
-  {
-    auto bmr_wire{
-        register_pointer->EmplaceWire<encrypto::motion::proto::bmr::Wire>(dummy_input, *backend)};
-    w = bmr_wire;
-    bmr_wire->GetMutablePublicKeys() = encrypto::motion::Block128Vector::MakeZero(
-        backend->GetConfiguration()->GetNumOfParties() * number_of_simd);
-    bmr_wire->GetMutableSecretKeys() = encrypto::motion::Block128Vector::MakeZero(number_of_simd);
-    bmr_wire->GetMutablePermutationBits() = encrypto::motion::BitVector<>(number_of_simd);
-    bmr_wire->SetSetupIsReady();
-    bmr_wire->SetOnlineFinished();
-  }
-
-  return encrypto::motion::ShareWrapper(
-      std::make_shared<encrypto::motion::proto::bmr::Share>(wires));
-}
-
-encrypto::motion::ShareWrapper DummyBooleanGmwShare(encrypto::motion::PartyPointer &party,
-                                                    std::size_t number_of_wires,
-                                                    std::size_t number_of_simd)
-{
-  std::vector<encrypto::motion::WirePointer> wires(number_of_wires);
-  const encrypto::motion::BitVector<> dummy_input(number_of_simd);
-
-  encrypto::motion::BackendPointer backend{party->GetBackend()};
-  encrypto::motion::RegisterPointer register_pointer{backend->GetRegister()};
-
-  for (auto &w : wires)
-  {
-    w = register_pointer->EmplaceWire<encrypto::motion::proto::boolean_gmw::Wire>(dummy_input,
-                                                                                  *backend);
-    w->SetOnlineFinished();
-  }
-
-  return encrypto::motion::ShareWrapper(
-      std::make_shared<encrypto::motion::proto::boolean_gmw::Share>(wires));
 }
 
 encrypto::motion::RunTimeStatistics EvaluateProtocol(
