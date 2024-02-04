93005065ced14709c06d29421862e06baa42d889
Schiller Jonas <q533604@europe.bmw.corp>
2024-02-04 15:25:01 +0100

    Changed Parameter Name

--- src/thesis/aes128_thesis/aes128_main.cpp
+++ src/thesis/aes128_thesis/aes128_main.cpp
@@ -55,7 +55,7 @@ int main(int ac, char *av[])
     if (help_flag)
       return EXIT_SUCCESS;
 
-    const auto number_of_simd{user_options["num-simd"].as<std::size_t>()};
+    const auto number_of_simd{user_options["simd"].as<std::size_t>()};
     const auto number_of_repetitions{user_options["repetitions"].as<std::size_t>()};
     encrypto::motion::MpcProtocol protocol;
     const std::string protocol_string{user_options["protocol"].as<std::string>()};
@@ -137,7 +137,7 @@ std::pair<program_options::variables_map, bool> ParseProgramOptions(int ac, char
       ("configuration-file,f", program_options::value<std::string>(), kConfigFileMessage.data())
       ("my-id", program_options::value<std::size_t>(), "my party id")
       ("parties", program_options::value<std::vector<std::string>>()->multitoken(), "info (id,IP,port) for each party e.g., --parties 0,127.0.0.1,23000 1,127.0.0.1,23001")
-      ("num-simd", program_options::value<std::size_t>()->default_value(1), "number of SIMD values for AES evaluation")
+      ("simd", program_options::value<std::size_t>()->default_value(1), "number of SIMD values for AES evaluation")
       ("protocol", program_options::value<std::string>()->default_value("BMR"), "Boolean MPC protocol (BMR or GMW)")
       ("online-after-setup", program_options::value<bool>()->default_value(true), "compute the online phase of the gate evaluations after the setup phase for all of them is completed (true/1 or false/0)")
       ("repetitions", program_options::value<std::size_t>()->default_value(1), "number of repetitions")
