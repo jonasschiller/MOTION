5739afb91281da33df0edc43aae0ed59c9460aad
Schiller Jonas <q533604@europe.bmw.corp>
2024-02-13 07:56:46 +0100

    Auctioning Bristol

---
+++ src/thesis/auction_hycc/common/auction.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "base/party.h"
+#include "statistics/run_time_statistics.h"
+
+encrypto::motion::RunTimeStatistics EvaluateProtocol(encrypto::motion::PartyPointer &party,
+                                                     std::size_t number_of_simd,
+                                                     encrypto::motion::MpcProtocol protocol);
---
+++ src/thesis/auction_hycc/common/auction_h.cpp
@@ -0,0 +1,31 @@
+#include "auction_h.h"
+
+#include "algorithm/algorithm_description.h"
+#include "protocols/bmr/bmr_wire.h"
+#include "protocols/boolean_gmw/boolean_gmw_wire.h"
+#include "protocols/share_wrapper.h"
+#include "statistics/analysis.h"
+#include "statistics/run_time_statistics.h"
+#include "utility/config.h"
+
+encrypto::motion::RunTimeStatistics EvaluateProtocol(encrypto::motion::PartyPointer &party,
+                                                     std::size_t number_of_simd,
+                                                     encrypto::motion::MpcProtocol protocol)
+{
+  // TODO tests
+  std::vector<encrypto::motion::BitVector<>> tmp(640,
+                                                 encrypto::motion::BitVector<>(1));
+  encrypto::motion::ShareWrapper input{
+      protocol == encrypto::motion::MpcProtocol::kBooleanGmw
+          ? party->In<encrypto::motion::MpcProtocol::kBooleanGmw>(tmp, 0)
+          : party->In<encrypto::motion::MpcProtocol::kBmr>(tmp, 0)};
+  const auto kPathToAlgorithm{std::string(encrypto::motion::kRootDir) +
+                              "/circuits/benchmarks/auction.bristol"};
+  const auto auction_algorithm{encrypto::motion::AlgorithmDescription::FromBristol(kPathToAlgorithm)};
+  const auto result{input.Evaluate(auction_algorithm)};
+  encrypto::motion::ShareWrapper output;
+  party->Run();
+  party->Finish();
+  const auto &statistics = party->GetBackend()->GetRunTimeStatistics();
+  return statistics.front();
+}
